9e9d2bcb039f65b44ccb0fd8daaeac2225a80fd9
Marcin Nowak <marcin.nowak.92@gmail.com>
2025-05-23 13:48:02 +0200

    Add top navigation menu

---
+++ src/app/shared/top-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-top-menu',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './top-menu.component.html'
+})
+export class TopMenuComponent {}
